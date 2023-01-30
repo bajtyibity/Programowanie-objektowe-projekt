48c68f45f2dbb972f109151b0bc1d40e8f39d3b5
bajtyibity <angelofgodcode@gmail.com>
2023-01-30 01:48:55 +0100

    conect views to admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Lekarz
+from .models import Pacjent
 # Register your models here.
+
+#dodanie modelu lekarza i pacjenta do strony admina
+admin.site.register(Lekarz)
+admin.site.register(Pacjent)
