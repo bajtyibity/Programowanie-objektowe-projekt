8477813e6255784233c0beb44858530013fd19e1
bajtyibity <angelofgodcode@gmail.com>
2023-01-30 01:10:39 +0100

    add serializer

---
+++ app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from app.models import Lekarz
+from app.models import Pacjent
+
+#serializer dla lekarza
+class Lekarzserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Lekarz
+        fields = ('lekarz_id','imie_pacjenta','nazwisko_pacjenta')
+
+#serializer dla pacjenta
+class Pacjentserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Pacjent
+        fields = ('pacjent_id','imie_pacjenta','nazwisko_pacjenta')
+
